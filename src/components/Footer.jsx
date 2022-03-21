c9316aaf3037a4f5bc85b841fdc0fedee6b87098
Huy Anh Ngo <tristanhuyanhngo@gmail.com>
2022-03-21 16:10:58 +0700

    Update Navbar
    Navbar: 70%

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export const Footer = () => {
   return (
     <div>
-      <h1>Footer</h1>
     </div>
   );
 };
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export const Routes = () => {
   return (
     <div>
-        <h1>Routes</h1>
     </div>
   )
 }
