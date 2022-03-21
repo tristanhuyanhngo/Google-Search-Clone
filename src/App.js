b092b5f485734db7069918d495125e85dc9ad62b
Huy Anh Ngo <tristanhuyanhngo@gmail.com>
2022-03-21 14:50:40 +0700

    Create Navbar, Footer, Routes
    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { Navbar } from './components/Navbar'; // Navbar inside the curly braces cuz we're using "export const" instead of "export defalt"
+import { Footer } from './components/Footer';
+import { Routes } from './components/Routes';
+
+console.log(Navbar);
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div>
-      <p className="text-sky-400/100">App</p>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 }
